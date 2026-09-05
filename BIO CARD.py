c19d4f758ff371d388ee66e27900048268ba8880
Aditya Singh Rana <imaditya0330@gmail.com>
2026-09-05 08:38:41 +0530

    Update and rename mini1.py to BIO CARD.py

---
+++ BIO CARD.py
@@ -0,0 +1,19 @@
+print("my bio card")
+naam = 'aditya'
+print("naam:",naam)
+age = 19
+print("age:",age)
+city = "dehradun"
+print("city:",city)
+hobby = "coding"
+print("hobby:",hobby)
+dream = "GSoc"
+print("dream:",dream)
+coder = True
+print("coder:",coder)
+
+
+
+
+
+
